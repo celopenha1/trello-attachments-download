ec0cff97ed3b2de3615e4930558bdd95a90316bc
Marcelo Filho <30932802+celopenha@users.noreply.github.com>
2021-04-29 12:13:00 -0300

    Update zipAttachments.js

--- middlewares/zipAttachments.js
+++ middlewares/zipAttachments.js
@@ -1,37 +1,27 @@
-const fs = require('fs');
-const https= require('https');
-const AdminZip = require('adm-zip');
-const axios = require('axios').default;
-
-
+const fs =                require('fs');
+const https=              require('https');
+const AdminZip =          require('adm-zip');
 const attachmentService = require('../services/attachments.services');
-
-
-
+// CREATING ZIP FILE FROM PATH FOLDER
 exports.createZipFromAttachments = async (req, res, next)=>{
-
+  // GET CARD ID FROM CLIENT-SIDE
   const { cardId } = req.params;
-
+  //TRANSFORM THE VALUE ON STRING
   const cardIdString = String(cardId);
-
-
-
-
-  const callback = err=> err? console.log(err) : console.log(`diretório: ${cardId} foi criado com sucesso`)
-
+  
+  const callback = err=> err? console.log(err) : console.log(`diretório: ${cardId} foi criado com sucesso`);
+  
   const file = new AdminZip();
 
   const directoryPath = `./${cardId}`;
-
+  // CHEF IF THE DIRECTORY ALREADY EXISTS
   fs.existsSync(directoryPath) 
   ? console.log('O diretório já existe')
   : fs.mkdir(directoryPath, callback)
 
+  // GET ATTACHMENT BASED ON ATTACHMENT URL RECEIVE FROM TRELLO API CALLS.
   const getAttachment = (attachment)=>{
-    console.log(attachment)
-
     return new Promise ((resolve, reject)=>{
-
       const materia = fs.createWriteStream(`./${cardId}/${attachment.name}`)
       https.get(attachment.url, (response)=>{
         response.pipe(materia);
@@ -44,15 +34,15 @@ exports.createZipFromAttachments = async (req, res, next)=>{
       })
     })
   }
-
-
+  // GET ALL ATTACHMENTS DATA FROM API FROM CARD-ID
   const attachments = await attachmentService.findCardAttachments(cardId);
-
+  // CREATE A PROMISE FOR EACH ATTACHMENT TO MAKE SURE ALL OF THEM IS OK
   let promises = attachments.map(attachment => {
-    return getAttachment(attachment).then(res => res).catch(error => console.log(error))
-  })
-
-  console.log(promises)
+    return getAttachment(attachment).then(res => res).catch(error => console.log(error));
+  });
+  // THIS IS THE GOOD WAY TO WAITING ALL FILES TRANSFERED IN THE LOCAL FOLDER DYNAMICALLY AND
+  // EXECUTE CALLBACK IN RIGHT TIME
+  // WHEN ALL PROMISES ARE RESOLVED, THIS CALLBACK WILL INVOKED
   Promise.all(promises).then( results => {
     file.addLocalFolder(cardIdString);
      file.writeZip(`${cardIdString}.zip`, (error)=> console.log(error));
