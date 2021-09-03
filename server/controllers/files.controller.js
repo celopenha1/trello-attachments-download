a90e5e9e1cf3a95436c2c17bfbc6eef54b61c42c
Marcelo Filho <niqcelo@gmail.com>
2021-09-02 23:42:33 -0300

    refact

--- server/controllers/files.controller.js
+++ server/controllers/files.controller.js
@@ -1,22 +1,50 @@
-const fs                = require('fs');
-const path              = require('path');
+const fs = require('fs');
+const path = require('path');
 
+const admZip = require('adm-zip');
+const zipFile = new admZip();
 
-module.exports = class File{
-  constructor(){
+module.exports = class File {
+  constructor() {
     this.folderPath = path.join(__dirname, '../', 'temp', `attachments`);
-    this.zipPath =    path.join(__dirname, '../', 'temp', `attachments.zip`);
+    this.zipPath = path.join(__dirname, '../', 'temp', `attachments.zip`);
   }
 
-  createFolder(){ fs.mkdir(this.folderPath, error => error ? console.log(error) : console.log(`diretório criado com sucesso!`))}
+  createFolder() { 
+    fs.mkdir(this.folderPath, error => error ? console.log(error) : console.log(`diretório criado com sucesso!`)) 
+  }
 
-  removeFiles(){
+  removeFiles() {
     const removeZipCallback = (error) => error ? console.log(error) : console.log('Zip deletado com sucesso!');
     const removeFolderCallback = (error) => error ? console.log(error) : console.log('Pasta deletada com sucesso');
-    
+
     fs.unlink(this.zipPath, removeZipCallback);
     fs.rmdir(this.folderPath, { recursive: true }, removeFolderCallback);
   }
+
+  zipFolderAttachments(req, res, next) {
+    zipFile.addLocalFolder(this.folderPath);
+    zipFile.writeZip(this.zipPath, e => console.log(e));
+
+  }
+
+  sendZipFile(req, res) {
+    res.writeHead(200, {
+      'Content-Type': 'application/octet-stream',
+      'Content-Disposition': 'attachment; filename=teste.zip'
+    });
+
+    var readStream = fs.createReadStream(this.zipPath);
+
+    readStream.on("open", () => {
+      readStream.pipe(res);
+    });
+    readStream.on("close", () => {
+      this.removeFiles();
+    });
+
+  }
+
 
 }
 
